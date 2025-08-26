d2caeea1de492b66e9f2d5bbaf3a45f5b17ec27b
Lucas Zani <zanin0503@gmail.com>
2025-08-26 14:29:45 -0300

    feat: other methodes added

--- __tests__/calculadora.test.js
+++ __tests__/calculadora.test.js
@@ -1,4 +1,4 @@
-import { somar } from "../src/calculadora.js";
+import { dividir, multiplicar, somar, subtrair } from "../src/calculadora.js";
 
 describe("Calculadora", () => {
     test("Deve somar dois números corretamente", () => {
@@ -8,9 +8,45 @@ describe("Calculadora", () => {
         const b = 7;
 
         //Act (Executar)
-        const resultado = somar(a, b);
+        const resultado = somar(a,b);
 
         //Assert (Verificar)
         expect(resultado).toBe(12)
+    }),
+    test("Deve subtrair dois números corretamente", () => {
+        //AAA
+        //Arrange (preparar)
+        const a = 5;
+        const b = 7;
+
+        //Act (Executar)
+        const resultado = subtrair(a,b);
+
+        //Assert (Verificar)
+        expect(resultado).toBe(-2)
+    })
+    test("Deve multiplicar dois números corretamente", () => {
+        //AAA
+        //Arrange (preparar)
+        const a = 5;
+        const b = 7;
+
+        //Act (Executar)
+        const resultado = multiplicar(a,b);
+
+        //Assert (Verificar)
+        expect(resultado).toBe(35)
+    })
+    test("Deve dividir dois números corretamente", () => {
+        //AAA
+        //Arrange (preparar)
+        const a = 10;
+        const b = 2;
+
+        //Act (Executar)
+        const resultado = dividir(a, b);
+
+        //Assert (Verificar)
+        expect(resultado).toBe(5)
     })
 })
--- src/calculadora.js
+++ src/calculadora.js
@@ -4,7 +4,16 @@ export function somar(a, b){
     return a + b;
 }
 
+// função para testar subtração
 export function subtrair(a, b) {
-    return a - b
+    return a - b;
+}
+
+export function multiplicar(a, b) {
+    return a * b;
+}
+
+export function dividir(a, b) {
+    return a / b;
 }
 
